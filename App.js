8546f1aa86f48dc364416563653aec0fb559453f
Erick Arauz <erick.aprende.is@gmail.com>
2022-10-18 18:26:07 -0600

    First Commit of my react native app
    This is my first react native developed with Expo Cli and tested into my personal android phone.

--- App.js
+++ App.js
@@ -1,20 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
+import Form from './src/components/Form';
+import Footer from './src/components/Footer';
+import ResultCalculation from './src/components/ResultCalculation';
+import colors from './src/utils/colors';
 
 export default function App() {
+  const [capital, setCapital] = useState(null);
+  const [interes, setInteres] = useState(null);
+  const [months, setMonths] = useState(null);
+  const [total, setTotal] = useState(null);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  useEffect(() => {
+    capital && interes && months ? calculate() : reset();
+  }, [capital, interes, months]);
+
+  const calculate = () => {
+    reset();
+    if (!capital || capital == 0) {
+      setErrorMessage('Añade la cantidad que quieres solicitar');
+    } else if (!interes || interes == 0) {
+      setErrorMessage('Añade el interes al prestamo');
+    } else if (!months) {
+      setErrorMessage('Selecciona los meses a pagar');
+    } else {
+      const i = interes / 100;
+      const fee = capital / ((1 - Math.pow(i + 1, -months)) / i);
+      setTotal({
+        monthlyFee: fee.toFixed(2).replace('.', ','),
+        totalPayable: (fee * months).toFixed(2).replace('.', ','),
+      });
+    }
+  };
+
+  const reset = () => {
+    setErrorMessage('');
+    setTotal(null);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.background} />
+        <Text style={styles.titleApp}>Cotizador de Prestamos</Text>
+        <Form
+          setCapital={setCapital}
+          setInteres={setInteres}
+          setMonths={setMonths}
+          months={months}
+        />
+      </SafeAreaView>
+
+      <ResultCalculation
+        errorMessage={errorMessage}
+        capital={capital}
+        interes={interes}
+        months={months}
+        total={total}
+      />
+
+      <Footer calculate={calculate} />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  safeArea: {
+    marginTop: StatusBar.currentHeight,
+    height: 290,
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  background: {
+    backgroundColor: colors.PRIMARY_COLOR,
+    height: 200,
+    width: '100%',
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+    zIndex: -1,
+    position: 'absolute',
+  },
+  titleApp: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginTop: 15,
   },
 });
